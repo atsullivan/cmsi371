5fcc6ac4cd4c6c858459037243438ebc71f3f3ab
atsullivan <atsullivan23@gmail.com>
2014-02-18 11:54:15 -0800

    cleaned up code

--- homework/toon/keyframe-tweening-demo.js
+++ homework/toon/keyframe-tweening-demo.js
@@ -48,7 +48,7 @@ $(function () {
             	draw: sprites.drawMrHankey,               
                 keyframes: [
                     {
-                        frame: 51,
+                        frame: 0,
                         tx: 0,
                         ty: 0,
                      //   ease: KeyframeTweener.linear
@@ -74,10 +74,6 @@ $(function () {
         width: canvas.width,
         height: canvas.height,
         sprites: racers,
-        
-        
-        
-        
         background: function (renderingContext) {
             var streetR = 86,
                 streetG = 92,
@@ -88,27 +84,7 @@ $(function () {
             var  mtnR = 20,
                  mtnG = 140,
                  mtnB = 62;  
-            
-            
-            
-            var baseR = 92,
-                baseG = 120,
-                baseB = 71;
-            var groundR = 94,
-                groundG = 71,
-                groundB = 16;
-            var treeR = 84,
-                treeG = 65,
-                treeB = 29;
-
-            renderingContext.save();
-//            var greenBase = renderingContext.createLinearGradient(0,canvas.height,canvas.width,canvas.height);
-//                greenBase.addColorStop(0, "rgb(" + (baseR-30) + "," + (baseG-30) + "," + (baseB-30) + ")");
-//                greenBase.addColorStop(1, "rgb(" + (baseR*2) + "," + (baseG*3) + "," + (baseB*2) + ")");
-//            renderingContext.fillStyle = greenBase;                
-                
-                
-			
+            renderingContext.save();	
 			renderingContext.fillStyle = "rgb(" + skyR + "," + skyG + "," + skyB + ")";
             renderingContext.fillRect(0, 0, canvas.width, canvas.height);
             
@@ -132,43 +108,13 @@ $(function () {
             renderingContext.moveTo(canvas.width-300, 300);
             renderingContext.lineTo(canvas.width, 100);
             renderingContext.lineTo(canvas.width, canvas.height);
-            renderingContext.fill();
-
-
-
-            
+            renderingContext.fill();    
             renderingContext.fillStyle = "white";
-            renderingContext.fillRect(0, 300, canvas.width, canvas.height);
-            
+            renderingContext.fillRect(0, 300, canvas.width, canvas.height);            
             renderingContext.fillStyle = "rgb(" + streetR + "," + streetG + "," + streetB + ")";
             renderingContext.fillRect(0, 450, canvas.width, 500);
-            
             renderingContext.fillStyle = "white";
             renderingContext.fillRect(0, 550, canvas.width, canvas.height);
-
-//            var groundLight = renderingContext.createLinearGradient(0, 350, canvas.width, 500);
-//                groundLight.addColorStop(0,"rgb(" + (groundR-50) + "," + (groundG-50) + "," + (groundB-20) + ")");
-//                groundLight.addColorStop(1,"rgb(" + groundR*2 + "," + groundG*2 + "," + groundB*4 + ")");
-//            renderingContext.fillStyle = groundLight;
-//            renderingContext.fillRect(0, 350, canvas.width, 500);
-//
-//            var treeLight = renderingContext.createLinearGradient(0, 300, 40, 80);
-//                treeLight.addColorStop(0, "rgb(" + (treeR-10) + "," + (treeG-10) + "," + (treeB-10) + ")");
-//                treeLight.addColorStop(1, "rgb(" + (treeR*2) + "," + (treeG*2) + "," + (treeB*2) + ")");
-//            renderingContext.fillStyle = treeLight;
-//            renderingContext.fillRect(20, 0, 20, 400);
-//            renderingContext.fillRect(70, 0, 28, 370);
-//            renderingContext.fillRect(200, 0, 18, 385);
-//            renderingContext.fillRect(290, 0, 15, 360);
-//            renderingContext.fillRect(370, 0, 16, 355);
-//            renderingContext.fillRect(575, 0, 20, 400);
-//            renderingContext.fillRect(670, 0, 26, 420);
-//            renderingContext.fillRect(700, 0, 22, 370);
-//            renderingContext.fillRect(765, 0, 30, 440);
-//            renderingContext.fillRect(820, 0, 55, 500);
-//            renderingContext.fillRect(870, 0, 12, 355);
-//            renderingContext.fillRect(950, 0, 20, 390);
-
             renderingContext.restore();
         }
 
