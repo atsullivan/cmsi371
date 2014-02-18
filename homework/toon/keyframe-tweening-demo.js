e4eca9c59563b109b8925325f3defa5df55e3b28
atsullivan <atsullivan23@gmail.com>
2014-02-17 22:42:43 -0800

    messing with kyles movement

--- homework/toon/keyframe-tweening-demo.js
+++ homework/toon/keyframe-tweening-demo.js
@@ -7,7 +7,6 @@ $(function () {
         racers = [
             {
             	draw: sprites.drawKyle,               
-                //draw: square,
                 keyframes: [
                     {
                         frame: 0,
@@ -17,54 +16,62 @@ $(function () {
                     },
 
                     {
-                        frame: 7,
-                        tx: 100,
+                        frame: 40,
+                        tx: 30,
                         ty: 50,
-                      // ease: KeyframeTweener.linear
-                       // ease: KeyframeTweener.quadEaseInOut
-                    },
-
-                    // The last keyframe does not need an easing function.
-                    {
-                        frame: 80,
-                        tx: 80,
-                        ty: 500,
-                   //     rotate: 60 // Keyframe.rotate uses degrees.
+                        ease: KeyframeTweener.linear
                     }
                 ]
             },
 
-            {
-            	draw: sprites.drawStan,
+//            {
+//            	draw: sprites.drawStan,
+//            	
+//            	keyframes: [
+//                    {
+//                        frame: 80,
+//                        tx: 20,
+//                        ty: 50,
+//                        ease: KeyframeTweener.linear
+//                    },
+//
+//                    {
+//                        frame: 120,
+//                        tx: 20,
+//                        ty: 0,
+//                      //  ease: KeyframeTweener.linear
+//                    }
+//                ]
+
                 //draw: circle,
-                keyframes: [
-                    {
-                        frame: 50,
-                        tx: 300,
-                        ty: 600,
-                        sx: 0.5,
-                        sy: 0.5,
-                        ease: KeyframeTweener.quadEaseOut
-                    },
-
-                    {
-                        frame: 100,
-                        tx: 300,
-                        ty: 0,
-                        sx: 3,
-                        sy: 0.25,
-                        ease: KeyframeTweener.quadEaseOut
-                    },
-
-                    {
-                        frame: 150,
-                        tx: 300,
-                        ty: 600,
-                        sx: 0.5,
-                        sy: 0.5
-                    }
-                ]
-            }
+//                keyframes: [
+//                    {
+//                        frame: 50,
+//                        tx: 300,
+//                        ty: 600,
+//                        sx: 0.5,
+//                        sy: 0.5,
+//                        ease: KeyframeTweener.quadEaseOut
+//                    },
+//
+//                    {
+//                        frame: 100,
+//                        tx: 300,
+//                        ty: 0,
+//                        sx: 3,
+//                        sy: 0.25,
+//                        ease: KeyframeTweener.quadEaseOut
+//                    },
+//
+//                    {
+//                        frame: 150,
+//                        tx: 300,
+//                        ty: 600,
+//                        sx: 0.5,
+//                        sy: 0.5
+//                    }
+//                ]
+            //}
         ];
 
     // Finally, we initialize the engine.  Mainly, it needs
