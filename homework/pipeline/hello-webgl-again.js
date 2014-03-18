6fb88b00614cb77fe70147d3389da3401ee6670d
atsullivan <atsullivan23@gmail.com>
2014-03-18 15:15:27 -0700

    cut down code

--- homework/pipeline/hello-webgl-again.js
+++ homework/pipeline/hello-webgl-again.js
@@ -124,51 +124,6 @@
 
     // Build the objects to display.
     objectsToDraw = [
-//        {
-//            vertices: [].concat(
-//                [ 0.0, 0.0, 0.0 ],
-//                [ 0.5, 0.0, -0.75 ],
-//                [ 0.0, 0.5, 0.0 ]
-//            ),
-//            colors: [].concat(
-//                [ 1.0, 0.0, 0.0 ],
-//                [ 0.0, 1.0, 0.0 ],
-//                [ 0.0, 0.0, 1.0 ]
-//            ),
-//            mode: gl.TRIANGLES
-//        },
-//
-//        {
-//            color: { r: 0.0, g: 1.0, b: 0 },
-//            vertices: [].concat(
-//                [ 0.25, 0.0, -0.5 ],
-//                [ 0.75, 0.0, -0.5 ],
-//                [ 0.25, 0.5, -0.5 ]
-//            ),
-//            mode: gl.TRIANGLES
-//        },
-//
-//        {
-//            color: { r: 0.0, g: 0.0, b: 1.0 },
-//            vertices: [].concat(
-//                [ -0.25, 0.0, 0.5 ],
-//                [ 0.5, 0.0, 0.5 ],
-//                [ -0.25, 0.5, 0.5 ]
-//            ),
-//            mode: gl.TRIANGLES
-//        },
-//
-//        {
-//            color: { r: 0.0, g: 0.0, b: 1.0 },
-//            vertices: [].concat(
-//                [ -1.0, -1.0, 0.75 ],
-//                [ -1.0, -0.1, -1.0 ],
-//                [ -0.1, -0.1, -1.0 ],
-//                [ -0.1, -1.0, 0.75 ]
-//            ),
-//            mode: gl.LINE_LOOP
-//        },
-
         {
             color: { r: 0.0, g: 0.5, b: 0.0 },
             vertices: Shapes.toRawTriangleArray(Shapes.cube2()),
@@ -256,7 +211,6 @@
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
         // Set up the rotation matrix.
-//        gl.uniformMatrix4fv(rotationMatrix, gl.FALSE, new Float32Array(getRotationMatrix(currentRotation, 0, 1, 0)));
         gl.uniformMatrix4fv(rotationMatrix, gl.FALSE, new Float32Array(getRotationMatrix(currentRotation, 1, 1, 0)));
 
 
