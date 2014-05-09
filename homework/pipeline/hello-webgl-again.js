30eb17644052b202af0162f29b07893cbc765252
atsullivan <asulli17@lion.lmu.edu>
2014-05-08 17:09:41 -0700

    Update hello-webgl-again.js

--- homework/pipeline/hello-webgl-again.js
+++ homework/pipeline/hello-webgl-again.js
@@ -1,23 +1,11 @@
-/*
- * For maximum modularity, we place everything within a single function that
- * takes the canvas that it will need.
- */
 (function (canvas) {
-
-    // Because many of these variables are best initialized then immediately
-    // used in context, we merely name them here.  Read on to see how they
-    // are used.
-    var gl, // The WebGL context.
-
+    var gl,
         // This variable stores 3D model information.
         objectsToDraw,
-
         // The shader program to use.
         shaderProgram,
-
         // Utility variable indicating whether some fatal has occurred.
         abort = false,
-
         // Important state variables.
         currentRotation = 0.0,
         currentInterval,
@@ -43,81 +31,10 @@
         j,
         maxj,
 
-        /*
-         * This code does not really belong here: it should live
-         * in a separate library of matrix and transformation
-         * functions.  It is here only to show you how matrices
-         * can be used with GLSL.
-         *
-         * Based on the original glRotate reference:
-         *     http://www.opengl.org/sdk/docs/man/xhtml/glRotate.xml
-         */
-        rotate = function (angle, x, y, z) {
-            // In production code, this function should be associated
-            // with a matrix object with associated functions.
-            var axisLength = Math.sqrt((x * x) + (y * y) + (z * z)),
-                s = Math.sin(angle * Math.PI / 180.0),
-                c = Math.cos(angle * Math.PI / 180.0),
-                oneMinusC = 1.0 - c,
-
-                // We can't calculate this until we have normalized
-                // the axis vector of rotation.
-                x2, // "2" for "squared."
-                y2,
-                z2,
-                xy,
-                yz,
-                xz,
-                xs,
-                ys,
-                zs;
-
-            // Normalize the axis vector of rotation.
-            x /= axisLength;
-            y /= axisLength;
-            z /= axisLength;
-
-            // *Now* we can calculate the other terms.
-            x2 = x * x;
-            y2 = y * y;
-            z2 = z * z;
-            xy = x * y;
-            yz = y * z;
-            xz = x * z;
-            xs = x * s;
-            ys = y * s;
-            zs = z * s;
-
-            // GL expects its matrices in column major order.
-            return [
-                (x2 * oneMinusC) + c,
-                (xy * oneMinusC) + zs,
-                (xz * oneMinusC) - ys,
-                0.0,
-
-                (xy * oneMinusC) - zs,
-                (y2 * oneMinusC) + c,
-                (yz * oneMinusC) + xs,
-                0.0,
-
-                (xz * oneMinusC) + ys,
-                (yz * oneMinusC) - xs,
-                (z2 * oneMinusC) + c,
-                0.0,
-
-                0.0,
-                0.0,
-                0.0,
-                1.0
-            ];
-        };
-
     // Grab the WebGL rendering context.
     gl = GLSLUtilities.getGL(canvas);
     if (!gl) {
         alert("No WebGL context found...sorry.");
-
-        // No WebGL, no use going on...
         return;
     }
 
@@ -158,8 +75,7 @@
                 );
             }
         }
-        objectsToDraw[i].colorBuffer = GLSLUtilities.initVertexBuffer(gl, objectsToDraw[i].colors);
-        console.log(objectsToDraw[i].normals);
+        objectsToDraw[i].colorBuffer = GLSLUtilities.initVertexBuffer(gl, objectsToDraw[i].colors);    
         objectsToDraw[i].normalBuffer = GLSLUtilities.initVertexBuffer(gl, objectsToDraw[i].normals);
     }
 
@@ -208,7 +124,6 @@
     // Lighting matrices initialized
     lightPosition = gl.getUniformLocation(shaderProgram, "lightPosition");
     lightDiffuse = gl.getUniformLocation(shaderProgram, "lightDiffuse");
-
 
     // Initialize projection matrix
     gl.uniformMatrix4fv(projectionMatrix, 
@@ -274,10 +189,7 @@
         gl.bindBuffer(gl.ARRAY_BUFFER, object.buffer);
         gl.vertexAttribPointer(vertexPosition, 3, gl.FLOAT, false, 0, 0);
         gl.drawArrays(object.mode, 0, object.vertices.length / 3);
-
-        // JD: You had the right idea here for one part of the shape-group
-        //     functionality.  But you need a little more...
-
+        
         // Recrusively draw subobjects/children of objects, if they exist
          if (object.subobjects && object.subobjects.length > 0) {
              for (var i = 0; i < subobjects.length; i++) {
@@ -294,7 +206,7 @@
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
         // Set up the rotation matrix.
-        gl.uniformMatrix4fv(rotationMatrix, gl.FALSE, new Float32Array(rotate(currentRotation, 1, 1, 0)));
+        //gl.uniformMatrix4fv(rotationMatrix, gl.FALSE, new Float32Array(rotate(currentRotation, 1, 1, 0)));
 
 
         // Display the objects.
@@ -307,26 +219,21 @@
     };
 
     // Set up our one light source and color.  Note the uniform3fv function.
-    gl.uniform3fv(lightPosition, [10.0, 10.0, 10.0]);
-    gl.uniform3fv(lightDiffuse, [1.0, 1.0, 1.0]);
+    gl.uniform3fv(lightPosition, [7.0, 10.0, 10.0]);
+    gl.uniform3fv(lightDiffuse, [1.0, 2.0, 1.0]);
+    gl.uniformMatrix4fv(rotationMatrix, gl.FALSE, new Float32Array((new Matrix4x4()).conversion()));
 
     // Draw the initial scene.
     drawScene();
 
-    // Set up the rotation toggle: clicking on the canvas does it.
-    $(canvas).click(function () {
-        if (currentInterval) {
-            clearInterval(currentInterval);
-            currentInterval = null;
-        } else {
-            currentInterval = setInterval(function () {
-                currentRotation += 1.0;
-                drawScene();
-                if (currentRotation >= 360.0) {
-                    currentRotation -= 360.0;
-                }
-            }, 30);
-        }
+     $(canvas).mousemove(function (event) {
+        var rotationY = Matrix4x4.rotate(event.pageX, 1, 0, 1);
+            rotationX = Matrix4x4.rotate(event.pageY, 0, 1, 0);
+            finalRotation = rotationY.multiply(rotationX);
+
+        gl.uniformMatrix4fv(rotationMatrix, gl.FALSE, 
+                            new Float32Array(finalRotation.conversion()));
+        drawScene();
     });
 
 }(document.getElementById("hello-webgl")));
